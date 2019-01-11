ebca101c6d784b20674f11de0c8593f6e4d55fd6
QAQNick <1354253661@qq.com>
2019-01-11 16:05:14 +0800

    day01

---
+++ C_NC_day01/runnian/runnian/test.c
@@ -0,0 +1,20 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include<stdio.h>
+
+int main()
+{
+	int year = 0;
+	int count = 0;
+	for (year = 1000; year <= 2000; year++)
+	{
+		if (year % 400 == 0 || year % 4 && year % 100 != 0)
+		{
+			printf("%d ", year);
+			count++;
+		}
+	}
+	printf("\ncount = %d", count);
+	system("pause");
+	return 0;
+}
---
+++ C_NC_day01/sushu/sushu/test.c
@@ -0,0 +1,21 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+	int i = 0;
+	int j = 0;
+	for (i = 101; i < 200; i += 2 )
+	{
+		for (j = 2; j < i; j++)
+		{
+			if (i % j == 0)
+				break;
+		}
+		if (j>sqrt(i))
+			printf("%d\n", i);
+	}
+	system("pause");
+	return 0;
+}
